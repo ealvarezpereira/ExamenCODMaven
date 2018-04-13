51358fc4cb215bbbc6da975dcad596ff72db580b
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-04-13 09:39:24 +0200

    Añadido método para recibir el tiempo en vigo

--- src/main/java/com/mycompany/examenmaven/Tiempo.java
+++ src/main/java/com/mycompany/examenmaven/Tiempo.java
@@ -5,12 +5,42 @@
  */
 package com.mycompany.examenmaven;
 
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
 
 /**
  *
  * @author quique
  */
 public class Tiempo {
+    static Document doc;
+    static String title,temp;
+    static Element temperatura, pronostico;
     
+
+
+    public static void sacarTiempo() {
+
+        try {
+             
+            //Iniciamos la conexion con la página web
+            doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+            //Recibimos el titulo
+            title = doc.title();
+             
+            //Buscamos las etiquetas con ese valor
+            temperatura = doc.select("div.today_nowcard-temp").first();
+            pronostico = doc.select("div.today_nowcard-phrase").first();
+            
+            //Mostramos todo por pantalla
+           JOptionPane.showMessageDialog(null, title+"\n"+temperatura.text()+"\n"+pronostico.text());
+
+        } catch (IOException ex) {
+            System.out.println("Excepcion al conectarse a la URL "+ex);
+        }
+        
+    }
     
 }
